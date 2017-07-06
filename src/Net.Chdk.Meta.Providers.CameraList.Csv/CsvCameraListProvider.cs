59bdd951a5418dbd376c2642576eb2e290309bcd
Dmitry Shechtman <damnian@gmail.com>
2017-07-07 01:31:40 +0300

    Add solution

---
+++ src/Net.Chdk.Meta.Providers.CameraList.Csv/CsvCameraListProvider.cs
@@ -0,0 +1,15 @@
+﻿using Net.Chdk.Meta.Model.CameraList;
+using Net.Chdk.Meta.Providers.CameraModel.Csv;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Net.Chdk.Meta.Providers.CameraList.Csv
+{
+    sealed class CsvCameraListProvider : CsvCameraProvider<ListPlatformData, ListRevisionData, ListSourceData>, ICameraListProvider
+    {
+        public IDictionary<string, ListPlatformData> GetCameraList(Stream stream)
+        {
+            return GetCameras(stream);
+        }
+    }
+}

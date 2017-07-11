5df7df012c1303e27250785f34b71c8f93635bea
Dmitry Shechtman <damnian@gmail.com>
2017-07-11 04:13:54 +0300

    Add product name

--- src/Net.Chdk.Meta.Providers.CameraList.Csv/CsvCameraListProvider.cs
+++ src/Net.Chdk.Meta.Providers.CameraList.Csv/CsvCameraListProvider.cs
@@ -7,7 +7,7 @@ namespace Net.Chdk.Meta.Providers.CameraList.Csv
 {
     sealed class CsvCameraListProvider : CsvCameraProvider<ListPlatformData, ListRevisionData, ListSourceData>, ICameraListProvider
     {
-        public IDictionary<string, ListPlatformData> GetCameraList(string path)
+        public IDictionary<string, ListPlatformData> GetCameraList(string path, string productName)
         {
             return GetCameras(path);
         }
